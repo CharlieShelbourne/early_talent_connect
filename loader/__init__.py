dc747f6f038ebec103fe132f54e928bc34448662
Charlie Shelbourne <charlie_shelbourne@hotmail.co.uk>
2021-02-08 20:03:27 +0000

    test working

--- loader/__init__.py
+++ loader/__init__.py
@@ -6,23 +6,13 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    test = req.params.get('test')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        test = req_body.get('test')
 
     if test:
         return func.HttpResponse(f"test: {test}")
@@ -31,3 +21,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a test in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+    
