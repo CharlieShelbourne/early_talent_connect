63d30eebce9d43c778d8a19c681d4dfd26817487
Charlie Shelbourne <charlie_shelbourne@hotmail.co.uk>
2021-02-08 19:29:59 +0000

    added test param

--- loader/__init__.py
+++ loader/__init__.py
@@ -7,6 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    name = req.params.get('test')
     if not name:
         try:
             req_body = req.get_json()
@@ -20,5 +21,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+    if test:
+        return func.HttpResponse(f"test: {test}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a test in the query string or in the request body for a personalized response.",
              status_code=200
         )
